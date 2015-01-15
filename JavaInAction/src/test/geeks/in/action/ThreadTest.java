9294e1ba3930e288db0dcc2bf487c4a087e96dc0
Manish Devraj <manish.devraj@gmail.com>
2015-01-15 13:24:12 +0530

    Produce Consumer Low Level

---
+++ JavaInAction/src/test/geeks/in/action/ThreadTest.java
@@ -0,0 +1,41 @@
+package geeks.in.action;
+
+import geeks.in.action.corejava.thread.ProduceConsumerLowLevel;
+
+import org.junit.Test;
+
+public class ThreadTest extends BaseTestCase {
+
+	@Test
+	public void producerConsumerLowLevel() throws InterruptedException {
+		final ProduceConsumerLowLevel produceConsumer = new ProduceConsumerLowLevel();
+		Thread producer = new Thread() {
+			@Override
+			public void run() {
+				try {
+					produceConsumer.produce();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+
+		Thread consumer = new Thread() {
+			@Override
+			public void run() {
+				try {
+					produceConsumer.consume();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+
+		producer.start();
+		consumer.start();
+
+		producer.join();
+		consumer.join();
+
+	}
+}
