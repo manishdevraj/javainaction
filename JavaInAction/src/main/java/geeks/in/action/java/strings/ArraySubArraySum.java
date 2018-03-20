2ccbdf9c50312128e6584f18615de31082521f43
Manish Devraj <manish.devraj@gmail.com>
2018-03-20 23:49:40 +0530

    arrays

---
+++ JavaInAction/src/main/java/geeks/in/action/java/strings/ArraySubArraySum.java
@@ -0,0 +1,36 @@
+package geeks.in.action.java.strings;
+
+import java.util.Arrays;
+
+/**
+ * Given an array, arr, we define the following:
+
+ •	A subarray of arr is an array composed from a contiguous block of arr's elements.
+ For example, the subarrays of arr = [1, 2, 3] are [1], [2], [3], [1, 2], [2, 3], and [1, 2, 3].
+ •	The subarray sum of arr is the sum of the elements of all subarrays of arr. For example, if arr = [1, 2, 3],
+ then the subarray sum is (1) + (2) + (3) + (1 + 2) + (2 + 3) + (1 + 2 + 3) = 20.
+
+ Complete the subarraySum function in the editor below. It has one parameter: an array of integers, arr.
+ The function must return a long integer denoting the subarray sum of arr.
+
+
+ * Created by manishdevraj on 20/03/18.
+ */
+public class ArraySubArraySum {
+
+    /**
+     * Input Format
+     Locked stub code in the editor reads the following input from stdin and passes it to the function:
+     The first line contains an integer, n, denoting the number of elements in arr.
+     Each line i of the n subsequent lines (where 0 ≤ i < n) contains an integer describing arri.
+
+     Constraints
+     •	1 ≤ n ≤ 2 × 105
+     •	1 ≤ arri ≤ 103, where 0 ≤ i < n
+
+     Output Format
+     The function must return a long integer denoting the subarray sum of arr.
+
+     */
+
+}
---
+++ JavaInAction/src/main/java/geeks/in/action/java/strings/TwinStringsEvenOdd.java
@@ -0,0 +1,45 @@
+package geeks.in.action.java.strings;
+
+/**
+ *
+ * Two strings, a and b, are said to be twins only if they can be made equivalent by performing some number of
+ * operations on one or both strings. There are two possible operations:
+
+ •	SwapEven: Swap a character at an even-numbered index with a character at another even-numbered index.
+ •	SwapOdd: Swap a character at an odd-numbered index with a character at another odd-numbered index.
+
+ For example, a = "abcd" and b = "cdab" are twins because we can make them equivalent by performing operations.
+ Alternatively, a = "abcd" and b = "bcda" are not twins (operations do not move characters between odd and even indices), and neither are a = "abc"and b = "ab" (no amount of operations will insert a 'c' into string b).
+
+ Complete the twins function in the editor below. It has two parameters:
+
+ 1.	An array of n strings named a.
+ 2.	An array of n strings named b.
+
+ The function must return an array of strings where each index i (0 ≤ i < n) contains the string Yes if ai
+ and bi are twins or the string Noif they are not.
+
+ * Created by manishdevraj on 20/03/18.
+ */
+public class TwinStringsEvenOdd {
+
+    /*
+    Input Format
+Locked stub code in the editor reads the following input from stdin and passes it to the function:
+The first line contains an integer, n, denoting the number of elements in a.
+Each line i of the n subsequent lines (where 0 ≤ i < n) contains a string describing ai.
+The next line contains an integer, n, denoting the number of elements in b.
+Each line i of the n subsequent lines (where 0 ≤ i < n) contains a string describing bi.
+
+Constraints
+•	1 ≤ n ≤ 103
+•	1 ≤ lengths of ai, bi ≤ 100
+•	ai and bi are not guaranteed to have the same length.
+•	Strings ai and bi contain lowercase letters only (i.e., a through z).
+
+Output Format
+The function must return an array of strings where each index i (0 ≤ i < n) contains the string Yes if ai
+and bi are twins or the string Noif they are not.
+
+     */
+}
